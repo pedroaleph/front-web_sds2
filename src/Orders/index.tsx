77da0613a84f3ebdeaa4cca2ba7859a52cae2cec
Pedro Aleph Gomes de Souza Vasconcelos <pedro.aleph.allen@gmail.com>
2021-01-07 22:19:40 -0300

    Add Routes as StepsHeader

---
+++ src/Orders/index.tsx
@@ -0,0 +1,13 @@
+import StepsHeader from './StepsHeader';
+import './styles.css';
+
+function Orders() {
+    return(
+        <div>
+            <h1 className="orders-container">
+                <StepsHeader />
+            </h1>
+        </div>
+    );
+}
+export default Orders;
