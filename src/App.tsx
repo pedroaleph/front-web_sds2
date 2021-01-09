1ccbe248bdfbf6b112f2cff1772867c197d41f35
Pedro Aleph Gomes de Souza Vasconcelos <pedro.aleph.allen@gmail.com>
2021-01-09 00:17:56 -0300

    finish front-web

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Routes from './Routes';
 
 function App() {
   return (
+    <>
       <Routes />
-
+      <ToastContainer />
+    </>
   );
 }
 
